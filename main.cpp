36d1e08b72e00cc213c45228a1034983206cd753
ElianMiguel <elianferreira266@gmail.com>
2026-05-25 14:57:31 -0300

    estrutura inicial dos arquivos

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Game.hpp"
+#include "GamesDatabase.hpp"
+#include "Trie.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
