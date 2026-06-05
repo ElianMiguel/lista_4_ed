3d56b6fa03da7f58e60ec05cfc5fe5b5f1d68921
ElianMiguel <elianferreira266@gmail.com>
2026-06-05 17:51:07 -0300

    pequenas implementações

--- Trie.cpp
+++ Trie.cpp
@@ -22,12 +22,48 @@ Trie::Trie() {
 
 Trie::~Trie() {}
 
-bool Trie::insert(Game* game) {}
+std::string Trie::toSearchKey(std::string text) {
+    char* ptr = text.data();
+    while (*ptr != '\0') {
+        if (int(*ptr) == 32) {
+            ptr++;
+        }
+        if (int(*ptr) >= 65 && int(*ptr) <= 90) {
+            *ptr += 32;
+        }
+    }
 
-bool Trie::contains(std::string title) {}
+    std::string new_text = ptr;
+    return new_text;
+}
+
+bool Trie::insert(Game* game) {
+    TrieNode* current = this->root;
+    std::string title = toSearchKey(game->getTitle());
+    for (char c : title) {
+        if (current->children == nullptr) {
+            TrieNode* node = new TrieNode();
+            if (int(c) >= 48 && int(c) <= 57) {
+                current->children[25 + c - '0'];
+            }
+            current->children[c - 'a'] = node;
+        }
+
+        if (int(c) >= 48 && int(c) <= 57) {
+            current = current->children[25 + c - '0'];
+        }
+        current = current->children[c - 'a'];
+    }
+
+    current->isEndOfTitle = true;
+    current->game = game;
+    return true;
+}
+
+bool Trie::contains(std::string title) {
+
+}
 
 std::vector<Game*> Trie::autocomplete(std::string prefix, int k) {}
-
-std::string Trie::toSearchKey(std::string text) {}
 
 void Trie::sortResults(std::vector<Game*>& games) {}
